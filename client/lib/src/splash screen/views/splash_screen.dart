cde25292b268aac37f54d5a8f065427f4d95fb04
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-07-22 16:54:27 +0500

    feat(client): splash screen and get storage
    - Created Splash Screen
    - Initialized get storage
    - Making status bar transparent
    - Making route for onboarding screen
    - Change "entry point" to "home screen"

--- client/lib/src/splash screen/views/splash_screen.dart
+++ client/lib/src/splash screen/views/splash_screen.dart
@@ -1,10 +1,51 @@
+import 'package:fashionext/core/common/services/storage.dart';
+import 'package:fashionext/core/common/utils/app_colors.dart';
+import 'package:fashionext/core/const/resource.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    _navigate();
+    super.initState();
+  }
+
+  _navigate() async {
+    await Future.delayed(
+      const Duration(milliseconds: 3000),
+      () {
+        if (Storage().getBool('firsttime') == null) {
+          // Navigate to OnBoarding Screen
+          GoRouter.of(context).go('/onboarding');
+        } else {
+          // Navigate to Home Screen
+          GoRouter.of(context).go('/home');
+        }
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.kWhite,
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(R.ASSETS_IMAGES_SPLASHSCREEN_PNG),
+          ),
+        ),
+      ),
+    );
   }
 }
